f78389cf5476e048522653b37e03e04ba83b4ebf
sunitsen <69672778+sunitsen@users.noreply.github.com>
2023-07-03 13:02:46 +0600

    Update template.js

--- assets/template/js/template.js
+++ assets/template/js/template.js
@@ -1,24 +1,24 @@
-let locationDiv = document.querySelector('.location-div')
-console.log(locationDiv)
-var sections = locationDiv.children
+let locationDiv = document.querySelector('.location-div');
+console.log(locationDiv);
+var sections = locationDiv.children;
 
 window.addEventListener('scroll', function () {
-  var visibleSection = getVisibleSection()
+  var visibleSection = getVisibleSection();
   if (visibleSection) {
-    var sectionId = visibleSection.id
-    var url =
-      'file:///C:/Users/NAZ Computer/Desktop/Template/index.html#' + sectionId
-    window.history.replaceState(null, null, url)
+    var sectionId = visibleSection.id;
+    
+    var url = window.location.href + '#' + sectionId;
+    window.history.replaceState(null, null, url);
   }
-})
+});
 
 function getVisibleSection () {
   for (var i = 0; i < sections.length; i++) {
-    var section = sections[i]
-    var rect = section.getBoundingClientRect()
+    var section = sections[i];
+    var rect = section.getBoundingClientRect();
     if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
-      return section
+      return section;
     }
   }
-  return null
+  return null;
 }
